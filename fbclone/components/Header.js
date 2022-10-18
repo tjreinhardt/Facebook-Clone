f50551e39b89dd63293cbfbf6a6fe16fd6b374e2
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-10-18 16:22:17 -0700

    facebook nextauth login works

--- fbclone/components/Header.js
+++ fbclone/components/Header.js
@@ -16,10 +16,12 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signOut, useSession } from 'next-auth/react';
 
 
 
 function Header() {
+  const { data: session, status } = useSession()
   return (
     <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
 
@@ -55,8 +57,16 @@ function Header() {
 
       <div className='flex items-center sm:space-x-2 justify-end'>
         {/* Profile Pic */}
+        <Image
+          onClick={signOut}
+          className="rounded-full cursor-pointer"
+          src={session.user.image}
+          width="40"
+          height="40"
+          layout='fixed'
+        />
 
-        <p className='whitespace-nowrap font-semibold pr-3'> Users Name </p>
+        <p className='whitespace-nowrap font-semibold pr-3'> {session.user.name} </p>
         <ViewGridIcon className='hidden xl:inline-flex p-2 h-10 w-10 bg-gray-200 rounded-full text-gray-700 cursor-pointer hover:bg-gray-300' />
         <ChatIcon className='hidden xl:inline-flex p-2 h-10 w-10 bg-gray-200 rounded-full text-gray-700 cursor-pointer hover:bg-gray-300' />
         <BellIcon className='hidden xl:inline-flex p-2 h-10 w-10 bg-gray-200 rounded-full text-gray-700 cursor-pointer hover:bg-gray-300' />
--- fbclone/pages/index.js
+++ fbclone/pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div>
       <Head>
@@ -18,4 +21,16 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  //get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
