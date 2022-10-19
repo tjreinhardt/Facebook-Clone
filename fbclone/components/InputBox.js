effe3b1f8910f642c1388e86a1561e820c6c46ee
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-10-19 12:18:56 -0700

    firebase added

--- fbclone/components/InputBox.js
+++ fbclone/components/InputBox.js
@@ -3,12 +3,30 @@ import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { EmojiHappyIcon } from "@heroicons/react/outline"
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid"
+import { useRef } from 'react'
+import { db } from '../firebase'
+import firebase from "../firebase"
+import { collection, addDoc } from 'firebase/firestore/lite'
+import { serverTimestamp } from 'firebase/firestore'
 
 function InputBox() {
   const { data: session } = useSession();
+  const inputRef = useRef(null);
+  const postsCollectionRef = collection(db, 'posts');
 
   const sendPost = (e) => {
     e.preventDefault()
+
+    if (!inputRef.current.value) return;
+
+    const docRef = addDoc(collection(db, "posts"), {
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image
+    })
+
+    inputRef.current.value = "";
   };
 
   return (
@@ -25,6 +43,7 @@ function InputBox() {
           <input
             className='rounded-full h-12 bg-gray-200 flex-grow px-5 focus:outline-none'
             type="text"
+            ref={inputRef}
             placeholder={`What's on your mind, ${session.user.name}?`}
           />
           <button hidden type="submit" onClick={sendPost}>Submit</button>
@@ -46,10 +65,7 @@ function InputBox() {
           <EmojiHappyIcon className='h-7 text-yellow-300' />
           <p className='text-xs sm:text-sm xl:text-base'> Live Video</p>
         </div>
-
       </div>
-
-
     </div>
   )
 }
